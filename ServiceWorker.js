77995990eb72ff60ce539b0a2239d1023532d6a1
Laurie Annis <laurieannis@yahoo.com>
2025-03-06 18:32:16 -0700

    First commit

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Unreality3D-3DViewer-0.1.0";
+const contentToCache = [
+    "Build/3DViewerBuilds.loader.js",
+    "Build/3DViewerBuilds.framework.js",
+    "Build/3DViewerBuilds.data",
+    "Build/3DViewerBuilds.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
