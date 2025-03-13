a9a1d485dcf94d76bb5f33e53ba76c798de652d9
Laurie Annis <laurieannis@yahoo.com>
2025-03-12 18:40:52 -0700

    removed all test code, rebuilt

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "Unreality3D-3DViewer-0.1.4";
+const cacheName = "Unreality3D-3DViewer-0.1.5";
 const contentToCache = [
     "Build/3DViewerBuilds.loader.js",
     "Build/3DViewerBuilds.framework.js",
